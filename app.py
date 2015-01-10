cda16ae8aef1626d44f236b2d03f904559592cdb
Jonathan Goldman <jonathan@unicyclelabs.com>
2015-01-10 10:18:00 -0800

    Initial template layout.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from flask import render_template
+
 from app_core import app
 import constants
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug = constants.DEBUG
