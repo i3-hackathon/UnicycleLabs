866dac8c132ef40bed8449be6846595fb8afaadd
Jonathan Goldman <jonathan@unicyclelabs.com>
2015-01-10 16:25:44 -0800

    Add basic search form.

--- static/js/script.js
+++ static/js/script.js
@@ -11,6 +11,11 @@ function currentDatetimeRoundedUp() {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate(), hour, minute);
 }
 
+function endTimeFromStartTime(startTime, durationHours) {
+    return new Date(startTime.getFullYear(), startTime.getMonth(),
+      startTime.getDate(), startTime.getHours() + durationHours, startTime.getMinutes());
+}
+
 function AppStateModel() {
   this.vehicleResults = null;
 
@@ -19,29 +24,104 @@ function AppStateModel() {
   };
 }
 
-function RootCtrl($scope, $appState, $http) {
-  $scope.appState = $appState;
+/*** Controllers ***/
 
-  $scope.dummySearch = function() {
-    var startTime = currentDatetimeRoundedUp();
-    var endTime = new Date(startTime.getFullYear(), startTime.getMonth(),
-      startTime.getDate(), startTime.getHours() + 2, startTime.getMinutes());
+function RootCtrl($scope, $appState) {
+  $scope.appState = $appState;
+}
+
+function SearchFormCtrl($scope, $appState, $http) {
+  $scope.gettingCurrentPosition = false;
+  $scope.searching = false;
+
+  var startTime = currentDatetimeRoundedUp();
+  var endTime = endTimeFromStartTime(startTime, 2);
+  $scope.form = {
+    rawLocation: null,
+    lat: null,
+    lng: null,
+    startTime: startTime,
+    endTime: endTime
+  };
+
+  $scope.locationInputFocused = function() {
+    if ($scope.form.rawLocation == 'Current location') {
+      $scope.form.rawLocation = null;
+    }
+  };
+
+  $scope.placeChanged = function(place) {
+    if (place['geometry'] && place['geometry']['location']) {
+      var location = place['geometry']['location'];
+      $scope.form.lat = location.lat();
+      $scope.form.lng = location.lng();
+    }
+  };
+
+  $scope.getCurrentLocation = function() {
+    $scope.gettingCurrentLocation = true;
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+      $scope.form.lat = position.coords.latitude;
+      $scope.form.lng = position.coords.longitude;
+      $scope.form.rawLocation = 'Current location';
+      $scope.gettingCurrentLocation = false;
+      $scope.$apply();
+    }, function() {
+      $scope.form.currentLocation = null;
+      $scope.gettingCurrentLocation = false;
+      $scope.$apply();
+    });
+  };
+
+  $scope.submit = function() {
+    $scope.searching = true;
     var params = {
-      'lat': 37.7735937,
-      'lng': -122.4036157,
-      'start_time': startTime.toISOString(),
-      'end_time': endTime.toISOString()
+      'lat': $scope.form.lat,
+      'lng': $scope.form.lng,
+      'start_time': $scope.form.startTime.toISOString(),
+      'end_time': $scope.form.endTime.toISOString()
     };
     $http.get('/search?' + $.param(params))
       .success(function(response) {
+        $scope.searching = false;
         $appState.vehicleResults = response['vehicles'];
       });
   };
+
+  $scope.getCurrentLocation();
 }
 
 function VehicleResultsCtrl($scope) {
 
 }
+
+/*** Directives ***/
+
+function htdGooglePlaceAutocomplete($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs, model) {
+      var placeChangeFn = $parse(attrs.onPlaceChange);
+      var types = attrs.locationTypes ? attrs.locationTypes.split(',') : [];
+      var options = {
+        types: types,
+        componentRestrictions: {}
+      };
+      var gPlace = new google.maps.places.Autocomplete(element[0], options);
+
+      google.maps.event.addListener(gPlace, 'place_changed', function() {
+        if (placeChangeFn) {
+          scope.$apply(function() {
+            placeChangeFn(scope, {$newPlace: gPlace.getPlace()});
+          });
+        };
+      });
+    }
+  };
+}
+
+/*** Bootstrapping ***/
 
 window['initApp'] = function() {
   function configuration($interpolateProvider) {
@@ -51,7 +131,9 @@ window['initApp'] = function() {
 
   angular.module('appModule', ['ui.bootstrap'], configuration)
     .controller('RootCtrl', RootCtrl)
+    .controller('SearchFormCtrl', SearchFormCtrl)
     .controller('VehicleResultsCtrl', VehicleResultsCtrl)
+    .directive('htdGooglePlaceAutocomplete', htdGooglePlaceAutocomplete)
     .value('$appState', new AppStateModel());
 
   angular.element(document).ready(function() {
